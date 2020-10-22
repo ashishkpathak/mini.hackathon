a759d93778f7348a5caae76b930bfd3fe5d3aee4
Ashish Pathak <ashish.pathak@optus.com.au>
2020-10-23 06:45:02 +1100

    JSch sample example.

---
+++ src/main/java/au/com/apathak/hackathon/scp/SFTPExample.java
@@ -0,0 +1,15 @@
+package au.com.apathak.hackathon.scp;
+
+public class SFTPExample {
+
+  public static void main(String[] args) {
+    final String remoteServer = "localhost";
+    final int remotePort =22;
+    final String remoteDirectory ="/tmp";
+    final String destinationFile="test.txt";
+
+
+
+  }
+
+}
