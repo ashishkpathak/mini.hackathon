5aaf61372cb932667b6398d0ee4f0cc0dadcc157
Ashish Pathak <ashish.pathak@optus.com.au>
2020-10-23 07:06:57 +1100

    WIP

--- src/main/java/au/com/apathak/hackathon/scp/SFTPExample.java
+++ src/main/java/au/com/apathak/hackathon/scp/SFTPExample.java
@@ -1,14 +1,29 @@
 package au.com.apathak.hackathon.scp;
 
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Logger;
+
 public class SFTPExample {
 
   public static void main(String[] args) {
     final String remoteServer = "localhost";
-    final int remotePort =22;
-    final String remoteDirectory ="/tmp";
-    final String destinationFile="test.txt";
+    final int remotePort = 22;
+    final String remoteDirectory = "/tmp";
+    final String destinationFile = "test.txt";
 
+    JSch jSch = new JSch();
+    JSch.setLogger(new Logger() {
+      @Override
+      public boolean isEnabled(int i) {
+        return true;
+      }
 
+      @Override
+      public void log(int i, String s) {
+        System.out.println(s);
+      }
+    });
+ 
 
   }
 
